c833958482220f7f01fccb3cebc7a83e18cd8e9e
nathanielwalker1 <nathanielwalkera@gmail.com>
2025-10-14 22:50:33 -0400

    fix 404 error

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        review: resolve(__dirname, 'review.html'),
+        about: resolve(__dirname, 'about.html'),
+        success: resolve(__dirname, 'success.html')
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
